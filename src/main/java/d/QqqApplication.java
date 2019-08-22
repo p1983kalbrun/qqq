179dc3d2cc4a5d84c997df97f0e21a6731783e22
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-08-22 16:37:33 +0100

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/d/QqqApplication.java
@@ -0,0 +1,12 @@
+package d;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqqApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(QqqApplication.class, args);
+	}
+
+}
